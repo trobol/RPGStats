56873642d52e817b68ae5519b37af3de33d91683
P03W <pweidman.mail@gmail.com>
2020-08-25 12:23:25 -0400

    Backport more fixes
    - CCA update
    - Mappings update (trivial)
    - Clean up components

--- src/main/java/mc/rpgstats/component/IStatComponent.java
+++ src/main/java/mc/rpgstats/component/IStatComponent.java
@@ -1,10 +1,9 @@
 package mc.rpgstats.component;
 
-import dev.onyxstudios.cca.api.v3.util.PlayerComponent;
 import nerdhub.cardinal.components.api.component.Component;
 import net.minecraft.entity.Entity;
 
-public interface IStatComponent extends PlayerComponent<Component> {
+public interface IStatComponent extends Component {
     int getXP();
     void setXP(int newXP);
     int getLevel();
