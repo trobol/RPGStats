c43a345e0cdf0039dcbcacacbb71a87d36f3214e
P03W <pweidman.mail@gmail.com>
2020-08-25 12:17:39 -0400

    Backport fishing fix to 1.16.1

--- src/main/java/mc/rpgstats/mixin/BonemealMixin.java
+++ src/main/java/mc/rpgstats/mixin/BonemealMixin.java
@@ -13,23 +13,20 @@ import net.minecraft.server.world.ServerWorld;
 import net.minecraft.tag.BlockTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.biome.BuiltInBiomes;
+import net.minecraft.world.biome.Biomes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.Objects;
-import java.util.Optional;
 import java.util.Random;
 
 @Mixin(BoneMealItem.class)
 public class BonemealMixin {
     private static final Random RANDOM = new Random();
-
+    
     @Inject(at = @At("HEAD"), method = "useOnGround")
     private static void groundEffectiveness(ItemStack stack, World world, BlockPos blockPos, Direction facing, CallbackInfoReturnable<Boolean> cir) {
         if (world.getBlockState(blockPos).isOf(Blocks.WATER) && world.getFluidState(blockPos).getLevel() == 8) {
@@ -40,7 +37,7 @@ public class BonemealMixin {
                     BlockPos blockPos2 = blockPos;
                     Biome biome = world.getBiome(blockPos);
                     BlockState blockState = Blocks.SEAGRASS.getDefaultState();
-
+                    
                     int k;
                     for (k = 0; k < i / (level / 50); ++k) {
                         blockPos2 = blockPos2.add(RANDOM.nextInt(3) - 1, (RANDOM.nextInt(3) - 1) * RANDOM.nextInt(3) / 2, RANDOM.nextInt(3) - 1);
@@ -49,22 +46,21 @@ public class BonemealMixin {
                             continue cursedLoop;
                         }
                     }
-    
-                    Optional<RegistryKey<Biome>> optional = world.method_31081(blockPos2);
-                    if (Objects.equals(optional, Optional.of(BuiltInBiomes.WARM_OCEAN)) || Objects.equals(optional, Optional.of(BuiltInBiomes.DEEP_WARM_OCEAN))) {
+                    
+                    if (biome == Biomes.WARM_OCEAN || biome == Biomes.DEEP_WARM_OCEAN) {
                         if (i == 0 && facing != null && facing.getAxis().isHorizontal()) {
                             blockState = BlockTags.WALL_CORALS.getRandom(world.random).getDefaultState().with(DeadCoralWallFanBlock.FACING, facing);
                         } else if (RANDOM.nextInt(4) == 0) {
                             blockState = BlockTags.UNDERWATER_BONEMEALS.getRandom(RANDOM).getDefaultState();
                         }
                     }
-
+                    
                     if (blockState.getBlock().isIn(BlockTags.WALL_CORALS)) {
                         for (k = 0; !blockState.canPlaceAt(world, blockPos2) && k < 4; ++k) {
                             blockState = blockState.with(DeadCoralWallFanBlock.FACING, Direction.Type.HORIZONTAL.random(RANDOM));
                         }
                     }
-
+                    
                     if (blockState.canPlaceAt(world, blockPos2)) {
                         BlockState blockState2 = world.getBlockState(blockPos2);
                         if (blockState2.isOf(Blocks.WATER) && world.getFluidState(blockPos2).getLevel() == 8) {
@@ -78,7 +74,7 @@ public class BonemealMixin {
             }
         }
     }
-
+    
     @Inject(at = @At("HEAD"), method = "useOnFertilizable")
     private static void onGrowable(ItemStack stack, World world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
         if (stack.getHolder() != null && stack.getHolder() instanceof ServerPlayerEntity) {
